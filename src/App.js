9582c1462f1a97fbac9d048c013caae481ecbc04
clebercmb <cleber@gmail.com>
2020-08-26 07:42:19 -0400

    test commit 2

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ export default function App() {
     
     let repositoryIndex = repositories.findIndex(repository => repository.id === id)
 
-
     //console.log('####==> repository=', newRepository)
     //newRepository.likes = response.data.likes
 
